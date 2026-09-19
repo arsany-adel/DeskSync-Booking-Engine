7d4b0cf10f36f61c17a5b3c75f96182e4ecaf7a8
Omar Amgad <omaramgadaboseif@gmail.com>
2026-09-19 20:54:02 +0300

    Refactor the Program.cs

--- backend/DeskSync.Api/Program.cs
+++ backend/DeskSync.Api/Program.cs
@@ -1,76 +1,40 @@
-using DeskSync.Api.Data;
-using DeskSync.Api.Entities;
-using DeskSync.Api.Repositories;
-using DeskSync.Api.Repositories.Interfaces;
-using DeskSync.Api.Services;
-using DeskSync.Api.Services.Interfaces;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
-using Hangfire;
-using Hangfire.PostgreSql;
 using DeskSync.Api.Configuration;
+using DeskSync.Api.Services.Interfaces;
+using Hangfire;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseNpgsql(connectionString, npgsqlOptions =>
-    {
-        npgsqlOptions.UseNodaTime(); 
-    });
-});
+builder.Services.AddPresentationServices();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
-
-builder.Services.Configure<WorkerSettingsOptions>(
-    builder.Configuration.GetSection(WorkerSettingsOptions.SectionName));
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());// Converts enums to strings in Swagger and JSON responses
-    });
 
 builder.Services.AddAuthentication(defaultScheme: "Bearer")
-    .AddBearerToken("Bearer");
-
+    .AddBearerToken("Bearer"); 
 builder.Services.AddAuthorization();
 
-builder.Services.AddProblemDetails();
-
-builder.Services.AddScoped<ITzdbSyncService, TzdbSyncService>();
-
-//Hangfire
-builder.Services.AddHangfire(config => config
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UsePostgreSqlStorage(options => 
-        options.UseNpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"))));
-
-builder.Services.AddHangfireServer();
-
 var app = builder.Build();
+
+app.UseExceptionHandler(); // Required for your ErrorOr mapping
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-//Hangfire
-app.UseHangfireDashboard("/hangfire");
+// Don't forget Hangfire Dashboard you must secure it with authentication and authorization in production
+
+
 RecurringJob.AddOrUpdate<ITzdbSyncService>(
-    "tzdb-daily-sync",//ID for the  background job so if the deployment is restarted, the job will not be duplicated
+    "tzdb-daily-sync",
     service => service.SyncReservationsAsync(CancellationToken.None),
-    Cron.Daily(2));// Runs daily at 2:00 AM UTC
+    Cron.Daily(2));
 
 app.Run();
